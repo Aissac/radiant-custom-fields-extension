79c31683aa345ba733aaeaed9bb221a36f9bce13
Cristi Duma <cristi.duma@aissac.ro>
2009-03-26 14:07:13 +0200

    modified views

--- public/javascripts/admin/custom_fields.js
+++ public/javascripts/admin/custom_fields.js
@@ -1,3 +1,5 @@
-Event.observe("show_custom_fields_link", 'click', function (){
-  $("cust_field").show();
-})
+Event.observe(window, 'load', function () {
+  Event.observe("show_custom_fields_link", 'click', function () {
+    $("custom_fields").show();
+  });
+});
---
+++ public/javascripts/admin/custom_fields_iframe.js
@@ -0,0 +1,22 @@
+Event.observe(window, 'load', function () {
+  Event.observe('enter_new_custom_field', 'click', function () {
+    $$("select.select_name")[0].hide();
+    $("enter_new_custom_field").hide();
+    $$("input.tb_name")[0].show();
+    $("cancel_enter_new_custom_field").show();
+  });
+  Event.observe('cancel_enter_new_custom_field', 'click', function () {
+    $$("select.select_name")[0].show();
+    $("enter_new_custom_field").show();
+    $$("input.tb_name")[0].hide();
+    $("cancel_enter_new_custom_field").hide();
+  });
+  Event.observe('and_new_custom_field_link', 'click', function () {
+    $("add_new_custom_field_div").show();
+    $("and_new_custom_field_link").hide();
+  });
+  Event.observe('cancel_new_custom_field', 'click', function () {
+    $("add_new_custom_field_div").hide();
+    $("and_new_custom_field_link").show();
+  })
+});
